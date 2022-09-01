9c2bbde2246572401315f7f1ca819921add161f1
teacher_cheater <johnnykayfun@gmail.com>
2022-09-01 14:56:40 +0300

    page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+// const URL = 'https://baconipsum.com/json-api/';
+// const URL = 'https://baconipsum.com/api/?type=meat-and-filler'
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="main">
+            <h1 className="main__title">Тренажёр слепой печати</h1>
+            <div className="main__window">
+                <div className="main__text">{text}</div>
+                <div className="main__content">
+                    <div className="speed"> speed:  </div>
+                    <div className="speed"> accuracy:  </div>
+
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
