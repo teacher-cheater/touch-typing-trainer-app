119dfb90449283ef94d734a4aa37733b1252b7e4
teacher_cheater <johnnykayfun@gmail.com>
2022-09-01 21:07:03 +0300

    add styles and query with text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import {useEffect, useState} from "react";
 
-
-// const URL = 'https://baconipsum.com/json-api/';
-// const URL = 'https://baconipsum.com/api/?type=meat-and-filler'
+const URL = 'https://baconipsum.com/api/?type=all-meat&sentences=4&start-with-lorem=1'
 
 
 function App() {
 
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        const controller = new AbortController();
+        fetch(URL, {
+            signal: controller.signal,
+        })
+            .then((data) => data.json())
+            .then((response) => setText(response))
+        return () => controller.abort(); //прерывание заапросов
+    }, []);
 
   return (
     <div className="App">
@@ -13,10 +23,21 @@ function App() {
             <h1 className="main__title">Тренажёр слепой печати</h1>
             <div className="main__window">
                 <div className="main__text">{text}</div>
+                    <span className="main__textarea"></span>
+                    <span className="main__passed"></span>
                 <div className="main__content">
-                    <div className="speed"> speed:  </div>
-                    <div className="speed"> accuracy:  </div>
-
+                    <div className="speed"> speed:
+                        <div className="main__result">
+                            <span> 7 </span>
+                            "зн/мин"
+                        </div>
+                    </div>
+                    <div className="accuracy"> accuracy:
+                        <div className="main__result">
+                            <span > 99 </span>
+                            "%"
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
