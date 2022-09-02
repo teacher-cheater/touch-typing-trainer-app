c812e0c12373ec34013985b05bede245c1f470f1
teacher_cheater <johnnykayfun@gmail.com>
2022-09-02 12:54:33 +0300

    keyboard event

--- src/App.js
+++ src/App.js
@@ -14,17 +14,29 @@ function App() {
         })
             .then((data) => data.json())
             .then((response) => setText(response))
-        return () => controller.abort(); //прерывание заапросов
+        return () => controller.abort(); //прерывание запросов
     }, []);
+
+
+    useEffect(() => {
+        const onKeypress = e => console.log(e);
+        document.addEventListener('keypress', onKeypress);
+        return () => {
+            document.removeEventListener('keypress', onKeypress);
+        };
+    }, []);
+
 
   return (
     <div className="App">
         <div className="main">
             <h1 className="main__title">Тренажёр слепой печати</h1>
             <div className="main__window">
-                <div className="main__text">{text}</div>
-                    <span className="main__textarea"></span>
-                    <span className="main__passed"></span>
+                <span id='myText' className="main__text"></span>
+                {/*{text.map((symbol, index) => (*/}
+                {/*        <span id='area' className="main__textarea" key={index}>{symbol}</span>*/}
+                {/*))}*/}
+                <span className="main__passed">{text}</span>
                 <div className="main__content">
                     <div className="speed"> speed:
                         <div className="main__result">
