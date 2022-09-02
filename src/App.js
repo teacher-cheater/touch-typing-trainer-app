1a06cbfc612a0bd5666d2d0bfafa5644c0c47d01
teacher_cheater <johnnykayfun@gmail.com>
2022-09-02 23:10:57 +0300

    split text, brought him out

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ const URL = 'https://baconipsum.com/api/?type=all-meat&sentences=4&start-with-lo
 
 function App() {
 
-    const [text, setText] = useState('')
+    const [text, setText] = useState([])
+    // console.log(text.join().split(''))
+
+    // const [rigthCount, setRigthCount] = useState();
+
+
 
     useEffect(() => {
         const controller = new AbortController();
@@ -13,13 +18,17 @@ function App() {
             signal: controller.signal,
         })
             .then((data) => data.json())
-            .then((response) => setText(response))
+            .then((response) => setText(response.join().split('')))
         return () => controller.abort(); //прерывание запросов
     }, []);
 
 
     useEffect(() => {
-        const onKeypress = e => console.log(e);
+        const onKeypress = e => {
+            console.log(e.key)
+
+        }
+
         document.addEventListener('keypress', onKeypress);
         return () => {
             document.removeEventListener('keypress', onKeypress);
@@ -27,34 +36,41 @@ function App() {
     }, []);
 
 
-  return (
-    <div className="App">
-        <div className="main">
-            <h1 className="main__title">Тренажёр слепой печати</h1>
-            <div className="main__window">
-                <span id='myText' className="main__text"></span>
-                {/*{text.map((symbol, index) => (*/}
-                {/*        <span id='area' className="main__textarea" key={index}>{symbol}</span>*/}
-                {/*))}*/}
-                <span className="main__passed">{text}</span>
-                <div className="main__content">
-                    <div className="speed"> speed:
-                        <div className="main__result">
-                            <span> 7 </span>
-                            "зн/мин"
+
+
+
+    return (
+        <div className="App">
+            <div className="main">
+                <h1 className="main__title">Тренажёр слепой печати</h1>
+                <div className="main__window">
+                    <span className="main__text">
+                        {text.map(letter =>
+                                <span className="main__textarea">
+                            {letter}
+                        {/* если текущий индекс меньше, чем состояние, то красить зеленым, и красить след индекс*/}
+                        </span>
+                        )}
+                    </span>
+                    <span className="main__passed"></span>
+                    <div className="main__content">
+                        <div className="speed"> speed:
+                            <div className="main__result">
+                                <span> 7 </span>
+                                "зн/мин"
+                            </div>
                         </div>
-                    </div>
-                    <div className="accuracy"> accuracy:
-                        <div className="main__result">
-                            <span > 99 </span>
-                            "%"
+                        <div className="accuracy"> accuracy:
+                            <div className="main__result">
+                                <span> 99 </span>
+                                "%"
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
